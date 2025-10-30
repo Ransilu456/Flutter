78d20b068a94d963ec2364f95d8404bb33715ba8
Ransilu456 <samarasekarakeshan@gmail.com>
2025-10-30 06:09:55 +0530

    Updated

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,122 +1,298 @@
 import 'package:flutter/material.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyWpsStyleApp());
+
+  doWhenWindowReady(() {
+    const initialSize = Size(1100, 700);
+    appWindow.minSize = const Size(800, 500);
+    appWindow.size = initialSize;
+    appWindow.alignment = Alignment.center;
+    appWindow.title = "WPS Style Flutter Window";
+    appWindow.show();
+  });
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyWpsStyleApp extends StatelessWidget {
+  const MyWpsStyleApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'WPS Style Flutter Desktop',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigoAccent),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const WpsMainWindow(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class WpsMainWindow extends StatelessWidget {
+  const WpsMainWindow({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return WindowBorder(
+      color: Colors.transparent,
+      width: 0,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: Scaffold(
+          backgroundColor: Colors.grey[50],
+          body: Stack(
+            children: [
+              // Drop shadow background (outer glow)
+              Container(
+                decoration: const BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0x33000000),
+                      blurRadius: 20,
+                      spreadRadius: 2,
+                      offset: Offset(0, 6),
+                    )
+                  ],
+                ),
+              ),
+
+              // Main content
+              Column(
+                children: const [
+                  CustomTitleBar(),
+                  Expanded(child: AppBody()),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class CustomTitleBar extends StatelessWidget {
+  const CustomTitleBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final buttonColors = WindowButtonColors(
+      iconNormal: Colors.white,
+      mouseOver: Colors.grey[700],
+      mouseDown: Colors.grey[800],
+      iconMouseOver: Colors.white,
+      iconMouseDown: Colors.white,
+    );
+
+    final closeButtonColors = WindowButtonColors(
+      mouseOver: Colors.redAccent,
+      mouseDown: Colors.red,
+      iconNormal: Colors.white,
+      iconMouseOver: Colors.white,
+    );
+
+    return Container(
+      height: 48,
+      decoration: BoxDecoration(
+        color: Colors.indigo[900],
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(16),
+          topRight: Radius.circular(16),
+        ),
+        boxShadow: const [
+          BoxShadow(
+            color: Color(0x22000000),
+            blurRadius: 8,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          const SizedBox(width: 16),
+          const Icon(Icons.folder_special, color: Colors.white, size: 20),
+          const SizedBox(width: 8),
+          const Text(
+            "WPS Flutter Window",
+            style: TextStyle(color: Colors.white, fontSize: 14),
+          ),
+          const SizedBox(width: 8),
+          Expanded(child: MoveWindow()), // Drag area
+          Row(
+            children: [
+              MinimizeWindowButton(colors: buttonColors),
+              MaximizeWindowButton(colors: buttonColors),
+              CloseWindowButton(colors: closeButtonColors),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AppBody extends StatefulWidget {
+  const AppBody({super.key});
+
+  @override
+  State<AppBody> createState() => _AppBodyState();
+}
+
+class _AppBodyState extends State<AppBody> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color(0xfff9fafc), Color(0xffedf1f8)],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Row(
+        children: [
+          // Sidebar
+          Container(
+            width: 200,
+            decoration: const BoxDecoration(
+              color: Color(0xff2b2d42),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0x22000000),
+                  blurRadius: 8,
+                  offset: Offset(2, 0),
+                ),
+              ],
+            ),
+            child: Column(
+              children: [
+                const SizedBox(height: 30),
+                const Text(
+                  "Navigation",
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+                const Divider(color: Colors.white38),
+                _navButton("Dashboard", Icons.dashboard),
+                _navButton("Settings", Icons.settings),
+                _navButton("About", Icons.info_outline),
+                const Spacer(),
+                _navButton("Exit", Icons.exit_to_app, color: Colors.redAccent),
+                const SizedBox(height: 20),
+              ],
+            ),
+          ),
+
+          // Main area
+          Expanded(
+            child: Center(
+              child: Container(
+                width: 400,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Color(0x22000000),
+                      blurRadius: 10,
+                      offset: Offset(0, 4),
+                    ),
+                  ],
+                ),
+                padding: const EdgeInsets.all(40),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      "WPS Office Style Counter",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      "$counter",
+                      style: const TextStyle(
+                        fontSize: 60,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.indigo,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        _button("−", Colors.redAccent, () {
+                          setState(() => counter--);
+                        }),
+                        const SizedBox(width: 20),
+                        _button("Reset", Colors.orange, () {
+                          setState(() => counter = 0);
+                        }),
+                        const SizedBox(width: 20),
+                        _button("+", Colors.green, () {
+                          setState(() => counter++);
+                        }),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _navButton(String title, IconData icon, {Color color = Colors.white}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+      child: InkWell(
+        onTap: () {},
+        borderRadius: BorderRadius.circular(8),
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+          decoration: BoxDecoration(
+            color: Colors.blueGrey[700],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Row(
+            children: [
+              Icon(icon, size: 18, color: color),
+              const SizedBox(width: 10),
+              Text(title, style: TextStyle(color: color)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _button(String label, Color color, VoidCallback onPressed) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 14),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        elevation: 6,
+      ),
+      child: Text(
+        label,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
     );
   }
 }
